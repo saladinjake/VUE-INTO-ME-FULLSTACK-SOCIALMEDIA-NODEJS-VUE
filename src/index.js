ec1a4f75e9ab6edd586db5c7ad720bf8dab6cc43
saladinjake <juwavictor@gmail.com>
2021-03-25 15:38:57 +0100

    Adds server classes and router class using encapsulation and oop technique

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
+/*
+* @AUTHOR: SALADIN JAKE ViCTOR DIBZ
+*@PROJECTNAME: Luvup
+*@DESCRIPTION : ONLINE SOCIAL MEDIA APPLICATION
+*
+*/
+///////////////////////////////////////////
+import  { App } from './server/App';   ///
+const LUVUP = new App();          ///
+LUVUP.run();                     ///
+export default LUVUP;           ///
+//////////////////////////////////////
 
-export default function hello(user = '    World') {
-  const u = user.trimStart().trimEnd();
-  return `Hello ${u}!\n`;
-}
 
-if (require.main === module) {
-  process.stdout.write(hello());
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// export default function hello(user = '    World') {
+//   const u = user.trimStart().trimEnd();
+//   return `Hello ${u}!\n`;
+// }
+//
+// if (require.main === module) {
+//   process.stdout.write(hello());
+// }
