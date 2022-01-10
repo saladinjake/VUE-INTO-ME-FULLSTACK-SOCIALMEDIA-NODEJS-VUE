137694774193f27cea69e27671036abb9c8b7dd4
saladinjake <juwavictor@gmail.com>
2022-01-10 03:40:47 +0100

    And helperlibraries

---
+++ backend/helpers/notification/Notification.js
@@ -0,0 +1,14 @@
+const Transport = require('../config/mail');
+
+/****************************************************************/
+/******* @author saladin jake (Victor juwa) ********************************/
+/******* @desc Notification Base Class : Povides resuable *******/
+/******* Methods for woking and sending notifications ***********/
+/****************************************************************/
+
+class Notification {
+  constructor() { }
+}
+
+
+module.exports = Notification;
